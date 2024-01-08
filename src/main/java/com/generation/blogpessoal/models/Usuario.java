7edda43ba3be9c2c91b224ddb42f8e26f7c84ff7
Breno Menezes <breno.luizmenezes0123@gmail.com>
2024-01-08 14:54:18 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/models/Usuario.java
+++ src/main/java/com/generation/blogpessoal/models/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -29,6 +30,7 @@ public class Usuario {
 	@NotNull(message = "O atributo nome é obrigatório!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo usuário é obrigatório!")
 	@Email(message = "O atributo usuário deve ser um email válido!")
 	private String usuario;
